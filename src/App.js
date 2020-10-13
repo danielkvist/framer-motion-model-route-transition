e87f39d27c7df55315314cf54a0b6a3b997801cf
Danielkvist <danielkvist@protonmail.com>
2020-10-13 16:30:15 +0200

    feat(app): wrap routes on AnimatePresence

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
 
 import Home from './pages/home';
 import Model from './pages/model';
 import Header from './components/header';
 import './App.scss';
+
+// AnimatePresence allows components to animate out when
+// they're removed from the React tree. It's required
+// to enable exit animations because React lacks lifecycle
+// methods that notifies components when they're going to
+// be unmounted and allows them to defer that unmounting
+// until after an operation is complete (in this case an
+// animation).
+//
+// initial prop only animates if we come from a direct link.
+// exitBeforeEnter prop allows only one component to be rendered at
+// the same time.
 
 function App() {
 	const imageDetails = {
@@ -18,18 +31,20 @@ function App() {
 
 			<Route
 				render={({ location }) => (
-					<Switch location={location} key={location.pathname}>
-						<Route
-							exact
-							path="/"
-							render={() => <Home imageDetails={imageDetails} />}
-						/>
-						<Route
-							exact
-							path="/model/:id"
-							render={() => <Model imageDetails={imageDetails} />}
-						/>
-					</Switch>
+					<AnimatePresence initial={false} exitBeforeEnter>
+						<Switch location={location} key={location.pathname}>
+							<Route
+								exact
+								path="/"
+								render={() => <Home imageDetails={imageDetails} />}
+							/>
+							<Route
+								exact
+								path="/model/:id"
+								render={() => <Model imageDetails={imageDetails} />}
+							/>
+						</Switch>
+					</AnimatePresence>
 				)}
 			/>
 		</Router>
